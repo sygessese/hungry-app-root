847eba6cc5c03405f86ee0e36755983e160a6500
Selam Gessese <sygessese@gmail.com>
2020-12-15 22:53:57 -0800

    Tweaked css

--- client/src/components/Restaurants.js
+++ client/src/components/Restaurants.js
@@ -9,7 +9,7 @@ const ListStyle = styled.div`
     width: 100%;
     height: 100%;
     align-items: center;
-    margin-bottom: 40em;
+    margin-bottom: 80em;
     scroll-snap-type: y proximity;
   `
 const ListItem = styled.div`
@@ -19,54 +19,47 @@ const ListItem = styled.div`
   width: 85%;
   margin-bottom: 2em;
   scroll-snap-stop: always;
-  // position: sticky;
-  // position: -webkit-sticky;
-  // top: 0;
   scroll-snap-align: start;
   background-color: ghostwhite;
-  scroll-margin: 10vh;
+  scroll-margin: 5vh;
 `
 const Titles = styled.div`
   color: ${props => props.footer ? "grey" : "chocolate"};
   font-size: ${props => props.footer ? "1.2em" : "1.4em"};
   line-height: ${props => props.footer ? "1.2em" : "1em"};
   margin-bottom: ${props => props.margin ? "2em" : "0em"};
+  margin-top: ${props => props.footer ? ".3em" : "0em"};
   font-weight: 400;
+  display: ${props => props.footer ? "flex" : "initial"};;
     `
 
 const Go = styled.button`
-font-size: 1em;
-background-color: burlywood;
+font-size: .9em;
 height: 1.2em;
-margin-left: 1m;
+border: 2px solid grey;
+border-radius: 10px;
+margin-right: .5em;
 `
 
 const Arrow = styled.span`
   font-size: 50px;
   position: absolute;
-  top: 10vh;
+  top: 5vh;
   left: 0;
   z-index: 1;
 `
-
 export default class Restaurants extends Component {
   render() {
-    // categories (titles), name, id, photos, price, rating, review_count
     var list = this.props.foods.length === 0 ? 'empty' :
       (this.props.foods).map((restaurant, id) => {
         return <ListItem key={id}>
-          <Titles>
-            {restaurant[4]} <b>{restaurant[1]}</b>
+          <Titles>{restaurant[4]} <b>{restaurant[1]}</b></Titles>
+          <Titles footer>{restaurant[5]}/5 by {restaurant[6]} foodies</Titles>
+          <img style={{ width: '100%', height: '100%' }} src={restaurant[3][0]} />
+          <Titles footer margin>
+            <Go onClick={() => this.props.select(restaurant[1], [restaurant[7], restaurant[8]])}>select</Go>
+            <div>{restaurant[0].join(' | ')}</div>
           </Titles>
-          <Titles footer>
-            {restaurant[5]}/5 by {restaurant[6]} foodies
-          </Titles>
-          <img
-            style={{ width: '100%', height: '100%' }}
-            src={restaurant[3][0]}
-          />
-          <Titles footer margin><Go onClick={() => this.props.select(restaurant[1], [restaurant[7], restaurant[8]])}>select</Go>{restaurant[0].join(' | ')}</Titles>
-
         </ListItem>
       });
 
